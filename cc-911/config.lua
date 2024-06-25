5af6480d7ccb229da719f1e7987bc9f1da4d480f
CollinTheDev <collin.entringer@gmail.com>
2024-06-25 12:15:48 -0500

    Update config.lua

--- cc-911/config.lua
+++ cc-911/config.lua
@@ -2,6 +2,6 @@ Config = {}
 
 Config.nearestPostalResourceName = "nearest-postal" -- Your Nearest Postal Name https://github.com/DevBlocky/nearest-postal
 
-Config.cadURL = "https://beta.communitycad.app" --  Your CAD URL No Trailng / at the end!
+Config.cadURL = "https://COMMUNITY.communitycad.app" --  Your CAD URL No Trailng / at the end!
 
-Config.api_key = 'bfEmqvjzXbsStMSFiyOKRz9b6ESN2QabFRjryY4LCg3zdEolaGQDv4AZIHB4vFfx' -- Your Community CAD API Key
+Config.api_key = 'API_KEY' -- Your Community CAD API Key
