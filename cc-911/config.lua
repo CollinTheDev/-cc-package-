d21ff6f0d9500fa902614019c09d11ff92d4f7f4
CollinTheDev <collin.entringer@gmail.com>
2024-06-25 11:15:23 -0500

    Add files via upload

---
+++ cc-911/config.lua
@@ -0,0 +1,7 @@
+Config = {}
+
+Config.nearestPostalResourceName = "nearest-postal" -- Your Nearest Postal Name https://github.com/DevBlocky/nearest-postal
+
+Config.cadURL = "https://beta.communitycad.app" --  Your CAD URL No Trailng / at the end!
+
+Config.api_key = 'bfEmqvjzXbsStMSFiyOKRz9b6ESN2QabFRjryY4LCg3zdEolaGQDv4AZIHB4vFfx' -- Your Community CAD API Key
---
+++ cc-911/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Mxrcy'
+description 'Community CAD 911 Script'
+version 'v1.0.2'
+
+
+shared_script 'config.lua'
+
+client_script 'client.lua'
+
+server_script 'server.lua'
